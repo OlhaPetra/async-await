5a20a87984241e05f1af73d2d7e2f646c6bba264
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-09-28 20:59:40 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
 //синтаксис async/await
 
 function getFruit(name) {
-    const fruits = {
-        strawberry: 'fresh berry',
-        kiwi: 'green fruit',
-        apple: 'red and big',
-    };
+  const fruits = {
+    strawberry: 'fresh berry',
+    kiwi: 'green fruit',
+    apple: 'red and big',
+  };
 
-    return new Promise(resolve => setTimeout(() => resolve(fruits[name]), 500));
+  return new Promise(resolve => setTimeout(() => resolve(fruits[name]), 500));
 }
-
-
 
 async function makeSmoothie() {
-    console.time('makeSmoothie')
-    const apple = await getFruit('apple')
-    console.log(apple)
+    try {
+        console.time('makeSmoothie');
+/*         const apple = await getFruit('apple');
+        const kiwi = await getFruit('kiwi');
+        const strawberry = await getFruit('strawberry'); */
+        //console.log(apple)
+        // console.log(kiwi)
+        //console.log(strawberry)
 
-    const kiwi = await getFruit('kiwi')
-    console.log(kiwi)
+        const apple = getFruit('apple');
+        const kiwi = getFruit('kiwi');
+        const strawberry = getFruit('strawberry');
 
-    const strawberry = await getFruit('strawberry')
-    console.log(strawberry)
-    console.timeEnd('makeSmoothie')
+         const fruit = await Promise.all([apple, kiwi, strawberry]);
+        console.log(fruit);
+        return fruit;
+
+        console.timeEnd('makeSmoothie');
+
+    } catch (error) {
+      console.log(error)
+    }
 }
 
-makeSmoothie()
+makeSmoothie().then(console.log);
 
- function getSmoothie() {
-    getFruit('apple').then(apple => {
-        console.log(apple)
-        getFruit('kiwi').then(kiwi => console.log(kiwi));
-    })
+function getSmoothie() {
+  getFruit('apple').then(apple => {
+    console.log(apple);
+    getFruit('kiwi').then(kiwi => console.log(kiwi));
+  });
 }
